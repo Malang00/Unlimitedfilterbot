45ac129742a7d1443923661385e830d5cb37ffad
Malang00 <91902331+Malang00@users.noreply.github.com>
2023-09-05 15:50:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -150,18 +150,18 @@ Nᴀᴍᴇ - {}"""
     MELCOW_ENG = """<b>Hᴇʟʟᴏ {} 😍, Aɴᴅ Wᴇʟᴄᴏᴍᴇ Tᴏ {} Gʀᴏᴜᴘ ❤️</b>"""
 
     SHORTLINK_INFO = """
-<b>──────「<a href=https://ziplinker.net> ᴇᴀʀɴ ᴍᴏɴᴇʏ</a> 」─────
+<b>──────「<a href=http://omegalinks.in> ᴇᴀʀɴ ᴍᴏɴᴇʏ</a> 」─────
 
 Now You can start earning 💸 money today with our Simple and easy-to-use bot!
 
 ›› Step 1: Add This bot to your group as an admin..
 
-›› Step 2: If you don't Using any shortner website then make account first on ziplinker.net (You can also use other link shortner website).
+›› Step 2: If you don't Using any shortner website then make account first on omegalinks.in (You can also use other link shortner website).
 
 ›› Step 3: Copy your API from website and then, simply set your website and API Using the
 /set_shortner command
 
-› Like this :  /set_shortner ziplinker.net b4d510e7b1e56da54f43c9e27569ee0a281121db
+› Like this :  /set_shortner omegalinks.in 75846756921608b60ca6a467cb368388197d712b
 
 
 ★ This bot will automatically converts links with Your Api and will provide your links.
